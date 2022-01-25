4facdbe1a778d9786a26c204b766c33bf4d5a5c5
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-25 12:56:28 +0900

    chore: build

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,28 +2,26 @@ import { test, expect } from 'vitest'
 import { removeProp, removeProps } from '../src/index'
 
 test('removeProp', () => {
-  t.deepEqual(removeProp({ a: 1, b: undefined }, undefined), { a: 1 } as any)
-  t.deepEqual(removeProp({ a: 1, b: undefined }, 1), { b: undefined } as any)
+  expect(removeProp({ a: 1, b: undefined }, undefined)).toEqual({ a: 1 })
+  expect(removeProp({ a: 1, b: undefined }, 1)).toEqual({ b: undefined })
 })
 test('removeProp empty objects', () => {
-  t.deepEqual(removeProp({}, {}), {})
-  t.deepEqual(removeProp({ a: {} }, {}), {} as any)
-  t.deepEqual(removeProp({ a: { b: { c: {} } } }, {}), {} as any)
-  t.deepEqual(removeProp({ a: { b: { c: { d: { e: {} } } } } }, {}), {} as any)
-  t.deepEqual(removeProp({ a: { b: { c: { d: { e: {} } } }, b2: {} } }, {}), {} as any)
-  t.deepEqual(removeProp({ a: { b: { c: { d: { e: {}, e2: {} } } }, b2: {} } }, {}), {} as any)
+  expect(removeProp({}, {})).toEqual({})
+  expect(removeProp({ a: {} }, {})).toEqual({})
+  expect(removeProp({ a: { b: { c: {} } } }, {})).toEqual({})
+  expect(removeProp({ a: { b: { c: { d: { e: {} } } } } }, {})).toEqual({})
+  expect(removeProp({ a: { b: { c: { d: { e: {} } } }, b2: {} } }, {})).toEqual({})
+  expect(removeProp({ a: { b: { c: { d: { e: {}, e2: {} } } }, b2: {} } }, {})).toEqual({})
 })
 test('removeProp empty objects and undefined', () => {
-  t.deepEqual(removeProps({}, [{}, undefined]), {})
-  t.deepEqual(removeProps({ a: {} }, [{}, undefined]), {} as any)
-  t.deepEqual(removeProps({ a: { b: { c: undefined } } }, [{}, undefined]), {} as any)
-  t.deepEqual(removeProps({ a: { b: { c: { d: { e: {} } } } } }, [{}, undefined]), {} as any)
-  t.deepEqual(
-    removeProps({ a: { b: { c: { d: { e: {} } } }, b2: undefined } }, [{}, undefined]),
-    {} as any
-  )
-  t.deepEqual(
-    removeProps({ a: { b: { c: { d: { e: {}, e2: undefined } } }, b2: {} } }, [{}, undefined]),
-    {} as any
-  )
+  expect(removeProps({}, [{}, undefined])).toEqual({})
+  expect(removeProps({ a: {} }, [{}, undefined])).toEqual({})
+  expect(removeProps({ a: { b: { c: undefined } } }, [{}, undefined])).toEqual({})
+  expect(removeProps({ a: { b: { c: { d: { e: {} } } } } }, [{}, undefined])).toEqual({})
+  expect(
+    removeProps({ a: { b: { c: { d: { e: {} } } }, b2: undefined } }, [{}, undefined])
+  ).toEqual({})
+  expect(
+    removeProps({ a: { b: { c: { d: { e: {}, e2: undefined } } }, b2: {} } }, [{}, undefined])
+  ).toEqual({})
 })
