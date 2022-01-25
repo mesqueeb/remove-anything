18bbd87b98ec8b08b3d7fd9e0263d8d9b0e2feeb
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-25 11:37:40 +0900

    build: cleanup 🧼

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,11 @@
-import test from 'ava'
+import { test, expect } from 'vitest'
 import { removeProp, removeProps } from '../src/index'
 
-test('removeProp', (t) => {
+test('removeProp', () => {
   t.deepEqual(removeProp({ a: 1, b: undefined }, undefined), { a: 1 } as any)
   t.deepEqual(removeProp({ a: 1, b: undefined }, 1), { b: undefined } as any)
 })
-test('removeProp empty objects', (t) => {
+test('removeProp empty objects', () => {
   t.deepEqual(removeProp({}, {}), {})
   t.deepEqual(removeProp({ a: {} }, {}), {} as any)
   t.deepEqual(removeProp({ a: { b: { c: {} } } }, {}), {} as any)
@@ -13,7 +13,7 @@ test('removeProp empty objects', (t) => {
   t.deepEqual(removeProp({ a: { b: { c: { d: { e: {} } } }, b2: {} } }, {}), {} as any)
   t.deepEqual(removeProp({ a: { b: { c: { d: { e: {}, e2: {} } } }, b2: {} } }, {}), {} as any)
 })
-test('removeProp empty objects and undefined', (t) => {
+test('removeProp empty objects and undefined', () => {
   t.deepEqual(removeProps({}, [{}, undefined]), {})
   t.deepEqual(removeProps({ a: {} }, [{}, undefined]), {} as any)
   t.deepEqual(removeProps({ a: { b: { c: undefined } } }, [{}, undefined]), {} as any)
